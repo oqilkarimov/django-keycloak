e95b68b767f9d452d24aeccab97c35a7f385d5b4
Karimov Oqil <karimov.oqil@gmail.com>
2023-03-11 17:22:19 +0500

    feat: Fetch username field

--- setup.py
+++ setup.py
@@ -2,51 +2,49 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = '0.2.4'
+VERSION = "0.2.5"
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-keycloak',
+    name="django-keycloak",
     version=VERSION,
     long_description=README,
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     extras_require={
-        'dev': [
-            'bumpversion==0.5.3',
-            'twine',
+        "dev": [
+            "bumpversion==0.5.3",
+            "twine",
         ],
-        'doc': [
-            'Sphinx==1.4.4',
-            'sphinx-autobuild==0.6.0',
-        ]
+        "doc": [
+            "Sphinx==1.4.4",
+            "sphinx-autobuild==0.6.0",
+        ],
     },
     setup_requires=[
-        'pytest-runner',
-        'python-keycloak-client-pkg==0.3.0',
+        "pytest-runner",
+        "python-keycloak-client-pkg==0.3.0",
     ],
-    
     install_requires=[
-        'python-keycloak-client-pkg==0.3.0',
-        'Django>=4.1',
+        "python-keycloak-client-pkg==0.3.0",
+        "Django>=4.1",
     ],
     tests_require=[
-        'pytest-django',
-        'pytest-cov',
-        'mock>=2.0',
-        'factory-boy',
-        'freezegun'
+        "pytest-django",
+        "pytest-cov",
+        "mock>=2.0",
+        "factory-boy",
+        "freezegun",
     ],
-    url='https://github.com/dabocs/django-keycloak',
-    license='MIT',
-    author='Ahmad Dabo',
-    author_email='dabo.cs@gmail.com',
-    description='Install Django Keycloak.',
-    classifiers=[]
-
+    url="https://github.com/dabocs/django-keycloak",
+    license="MIT",
+    author="Ahmad Dabo",
+    author_email="dabo.cs@gmail.com",
+    description="Install Django Keycloak.",
+    classifiers=[],
 )
--- src/django_keycloak/management/commands/keycloak_add_user.py
+++ src/django_keycloak/management/commands/keycloak_add_user.py
@@ -16,25 +16,32 @@ def realm(name):
     try:
         return Realm.objects.get(name=name)
     except Realm.DoesNotExist:
-        raise TypeError('Realm does not exist')
+        raise TypeError("Realm does not exist")
+
+
+def get_username_field() -> str:
+    UserModel = get_user_model()
+    if hasattr(UserModel, "USERNAME_FIELDS"):
+        return UserModel.USERNAME_FIELDS
+    return "username"
 
 
 def user(username):
     UserModel = get_user_model()
+    filter_qs = {get_username_field(): username}
     try:
-        return UserModel.objects.get(username=username)
+        return UserModel.objects.get(**filter_qs)
     except UserModel.DoesNotExist:
-        raise TypeError('User does not exist')
+        raise TypeError("User does not exist")
 
 
 class Command(BaseCommand):
-
     def add_arguments(self, parser):
-        parser.add_argument('--realm', type=realm, required=True)
-        parser.add_argument('--user', type=user, required=True)
+        parser.add_argument("--realm", type=realm, required=True)
+        parser.add_argument("--user", type=user, required=True)
 
     def handle(self, *args, **options):
-        user = options['user']
-        realm = options['realm']
+        user = options["user"]
+        realm = options["realm"]
 
         django_keycloak.services.users.add_user(client=realm.client, user=user)
